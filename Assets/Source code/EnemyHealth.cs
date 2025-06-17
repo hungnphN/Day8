842e50ed126241b98fb7babce3e073179e7c3c37
Hungnguyen <Hungphu2k2@gmail.com>
2025-06-17 21:23:07 +0700

    Demo 3

---
+++ Assets/Source code/EnemyHealth.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public GameObject explosionPrefab;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Die();
+    }
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
